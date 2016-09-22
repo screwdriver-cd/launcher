484cacacee48776a3c5d696858c611c0210b0bbf
Jeremiah Wuenschel <jeremiah.wuenschel@gmail.com>
2016-09-22 15:32:01 -0700

    Made the git helpers into methods of `repo` instead of being standalone

--- git/git.go
+++ git/git.go
@@ -19,6 +19,7 @@ type repo struct {
 	scmURL string
 	path   string
 	branch string
+	sha    string
 	logger io.Writer
 }
 
@@ -50,11 +51,11 @@ func (r repo) Path() string {
 
 // MergePR fetches and merges the specified pull request to the specified branch
 func (r repo) MergePR(prNumber string, sha string) error {
-	if err := fetchPR(prNumber, r.path, r.logger); err != nil {
+	if err := r.fetchPR(prNumber); err != nil {
 		return fmt.Errorf("fetching pr: %v", err)
 	}
 
-	if err := merge(sha, r.path, r.logger); err != nil {
+	if err := r.merge(sha); err != nil {
 		return fmt.Errorf("merging sha: %v", err)
 	}
 
@@ -64,44 +65,44 @@ func (r repo) MergePR(prNumber string, sha string) error {
 // Checkout checks out the git repo at the specified branch and configures the setting
 func (r repo) Checkout() error {
 	fmt.Fprintf(r.logger, "Cloning %v, on branch %v", r.scmURL, r.branch)
-	if err := clone(r.branch, r.scmURL, r.path, r.logger); err != nil {
+	if err := r.clone(); err != nil {
 		return fmt.Errorf("cloning repository: %v", err)
 	}
 
 	fmt.Fprintf(r.logger, "Setting user name and user email")
-	if err := setConfig("user.name", "sd-buildbot", r.path, r.logger); err != nil {
+	if err := r.setConfig("user.name", "sd-buildbot"); err != nil {
 		return fmt.Errorf("setting user name: %v", err)
 	}
 
-	if err := setConfig("user.email", "dev-null@screwdriver.cd", r.path, r.logger); err != nil {
+	if err := r.setConfig("user.email", "dev-null@screwdriver.cd"); err != nil {
 		return fmt.Errorf("setting user email: %v", err)
 	}
 
 	return nil
 }
 
-// clone clones a git repo into a destination directory
-func clone(branch, scmURL, destination string, logger io.Writer) error {
+// clone clones a git repo into its destination directory
+func (r repo) clone() error {
 	dir, err := os.Getwd()
 	if err != nil {
-		return fmt.Errorf("getting working directory: %v", err)
+		return err
 	}
-	return command(logger, dir, "clone", "--quiet", "--progress", "--branch", branch, scmURL, destination)
+	return command(r.logger, dir, "clone", "--quiet", "--progress", "--branch", r.branch, r.scmURL, r.path)
 }
 
-// setConfig sets the specified git settting
-func setConfig(setting, name, dir string, logger io.Writer) error {
-	return command(logger, dir, "config", setting, name)
+// setConfig applies a git config change to the repo
+func (r repo) setConfig(setting, name string) error {
+	return command(r.logger, r.path, "config", setting, name)
 }
 
 // fetchPR fetches a pull request
-func fetchPR(prNumber, dir string, logger io.Writer) error {
-	return command(logger, dir, "fetch", "origin", "pull/"+prNumber+"/head:pr")
+func (r repo) fetchPR(prNumber string) error {
+	return command(r.logger, r.path, "fetch", "origin", "pull/"+prNumber+"/head:pr")
 }
 
 // merge merges changes on the specified branch
-func merge(branch, dir string, logger io.Writer) error {
-	return command(logger, dir, "merge", "--no-edit", branch)
+func (r repo) merge(branch string) error {
+	return command(r.logger, r.path, "merge", "--no-edit", branch)
 }
 
 // command executes the git command
