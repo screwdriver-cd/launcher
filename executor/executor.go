8711ad5806ad14e19addf0fc47f1c3d4a0cf8f41
Min Zhang <minzhangcmu@gmail.com>
2017-12-11 10:35:08 -0800

    feat(473): always run teardown bookends (#133)
    * feat(473): always run teardown bookends
    
    * add tests

--- executor/executor.go
+++ executor/executor.go
@@ -10,6 +10,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/kr/pty"
 	"github.com/myesui/uuid"
@@ -17,6 +18,8 @@ import (
 )
 
 const (
+	// ExitLaunch is the exit code when a step fails to launch
+	ExitLaunch = 255
 	// ExitUnknown is the exit code when a step doesn't return an exit code (for some weird reason)
 	ExitUnknown = 254
 	// ExitOk is the exit code when a step runs successfully
@@ -96,7 +99,6 @@ func copyLinesUntil(r io.Reader, w io.Writer, match string) (int, error) {
 	return ExitOk, nil
 }
 
-// Source script file from the path
 func doRunCommand(guid, path string, emitter screwdriver.Emitter, f *os.File, fReader io.Reader) (int, error) {
 	executionCommand := []string{
 		"export SD_STEP_ID=" + guid,
@@ -109,6 +111,36 @@ func doRunCommand(guid, path string, emitter screwdriver.Emitter, f *os.File, fR
 	f.Write([]byte(shargs))
 
 	return copyLinesUntil(fReader, emitter, guid)
+}
+
+// Executes teardown commands
+func doRunTeardownCommand(cmd screwdriver.CommandDef, emitter screwdriver.Emitter, env []string, path string) (int, error) {
+	shargs := []string{"-e", "-c"}
+	shargs = append(shargs, cmd.Cmd)
+	c := exec.Command("sh", shargs...)
+
+	emitter.StartCmd(cmd)
+	fmt.Fprintf(emitter, "$ %s\n", cmd.Cmd)
+	c.Stdout = emitter
+	c.Stderr = emitter
+
+	c.Dir = path
+	c.Env = append(env, c.Env...)
+
+	if err := c.Start(); err != nil {
+		return ExitLaunch, fmt.Errorf("Launching command %q: %v", cmd.Cmd, err)
+	}
+
+	if err := c.Wait(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			waitStatus := exitError.Sys().(syscall.WaitStatus)
+
+			return waitStatus.ExitStatus(), ErrStatus{waitStatus.ExitStatus()}
+		}
+		return ExitUnknown, fmt.Errorf("Running command %q: %v", cmd.Cmd, err)
+	}
+
+	return ExitOk, nil
 }
 
 // Run executes a slice of CommandDefs
@@ -134,40 +166,67 @@ func Run(path string, env []string, emitter screwdriver.Emitter, build screwdriv
 
 	f.Write([]byte(shargs))
 
+	teardownFlag := false
+	var firstError error
+	var code int
+	var cmdErr error
+
 	cmds := build.Commands
 
 	for _, cmd := range cmds {
-		if err := api.UpdateStepStart(buildID, cmd.Name); err != nil {
-			return fmt.Errorf("Updating step start %q: %v", cmd.Name, err)
+		isTeardown, _ := regexp.MatchString("sd-teardown-*", cmd.Name)
+
+		// Start set up & user steps if previous steps succeed
+		if (!isTeardown) && (firstError == nil) {
+			if err := api.UpdateStepStart(buildID, cmd.Name); err != nil {
+				return fmt.Errorf("Updating step start %q: %v", cmd.Name, err)
+			}
+
+			// Create step script file
+			stepFilePath := "/tmp/step.sh"
+			if err := createShFile(stepFilePath, cmd); err != nil {
+				return fmt.Errorf("Writing to step script file: %v", err)
+			}
+
+			// Generate guid for the step
+			guid := uuid.NewV4().String()
+
+			// Set current running step in emitter
+			emitter.StartCmd(cmd)
+			fmt.Fprintf(emitter, "$ %s\n", cmd.Cmd)
+
+			fReader := bufio.NewReader(f)
+
+			code, cmdErr = doRunCommand(guid, stepFilePath, emitter, f, fReader)
+		} else if (isTeardown){
+			// Kill shell if first time switch to the teardown step
+			if !teardownFlag {
+				f.Write([]byte{4}) // EOT
+				teardownFlag = true
+			}
+
+			if err := api.UpdateStepStart(buildID, cmd.Name); err != nil {
+				return fmt.Errorf("Updating step start %q: %v", cmd.Name, err)
+			}
+
+			// Run teardown commands
+			code, cmdErr = doRunTeardownCommand(cmd, emitter, env, path)
 		}
 
-		// Create step script file
-		stepFilePath := "/tmp/step.sh"
-		if err := createShFile(stepFilePath, cmd); err != nil {
-			return fmt.Errorf("Writing to step script file: %v", err)
-		}
-
-		// Generate guid for the step
-		guid := uuid.NewV4().String()
-
-		// Set current running step in emitter
-		emitter.StartCmd(cmd)
-		fmt.Fprintf(emitter, "$ %s\n", cmd.Cmd)
-
-		fReader := bufio.NewReader(f)
-
-		// Execute command
-		code, cmdErr := doRunCommand(guid, stepFilePath, emitter, f, fReader)
 		if err := api.UpdateStepStop(buildID, cmd.Name, code); err != nil {
 			return fmt.Errorf("Updating step stop %q: %v", cmd.Name, err)
 		}
 
-		if cmdErr != nil {
-			return cmdErr
+		// Set first error flag
+		if (firstError == nil) && (cmdErr != nil) {
+			firstError = cmdErr
 		}
 	}
 
-	f.Write([]byte{4}) // EOT
+	// Return the first error occured
+	if (firstError != nil) {
+		return firstError
+	}
 
 	return nil
 }
