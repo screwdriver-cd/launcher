081e4c6000904e1622182950527e97359adc84e3
Min Zhang <minzhangcmu@gmail.com>
2018-09-06 15:32:16 -0700

    fix(979): fix entrypoint mkfifo cmd with proper user (#215)

--- Docker/launcher_entrypoint.sh
+++ Docker/launcher_entrypoint.sh
@@ -1,8 +1,22 @@
 #!/bin/sh
 
+I_AM_ROOT=false
+
+if [ `whoami` = "root" ]; then
+    I_AM_ROOT=true
+fi
+
+smart_run () {
+    if ! $I_AM_ROOT; then
+        sudo $@
+    else
+        $@
+    fi
+}
+
 # Create FIFO for emitter
 # https://github.com/screwdriver-cd/screwdriver/issues/979
-mkfifo -m 666 /opt/sd/emitter
+smart_run mkfifo -m 666 /opt/sd/emitter
 
 # Entrypoint
 /opt/sd/tini -- /bin/sh -c "$@"
