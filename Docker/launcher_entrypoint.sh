1ae058ca2bd284d8f9ca0e67f0ec85fe2302e73d
Min Zhang <minzhangcmu@gmail.com>
2018-09-06 15:20:28 -0700

    fix: fix entrypoint mkfifo cmd with proper user
    if the user of the image is not root, we need to do `sudo mkfifo -m 666 /opt/sd/emitter` to create it

--- Docker/launcher_entrypoint.sh
+++ Docker/launcher_entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+smart_run () {
+    if ! $I_AM_ROOT; then
+        sudo $@
+    else
+        $@
+    fi
+}
+
 # Create FIFO for emitter
 # https://github.com/screwdriver-cd/screwdriver/issues/979
-mkfifo -m 666 /opt/sd/emitter
+smart_run mkfifo -m 666 /opt/sd/emitter
 
 # Entrypoint
 /opt/sd/tini -- /bin/sh -c "$@"
