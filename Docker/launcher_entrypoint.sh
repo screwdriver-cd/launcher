719d84de5b202b116523477af21ca7e6539bb294
Min Zhang <minzhangcmu@gmail.com>
2018-09-07 11:02:45 -0700

    fix: change the emitter path
    cannot mkfifo /opt/sd/emitter for k8s-vm since the share mount /opt/sd is read-only

--- Docker/launcher_entrypoint.sh
+++ Docker/launcher_entrypoint.sh
@@ -16,7 +16,7 @@ smart_run () {
 
 # Create FIFO for emitter
 # https://github.com/screwdriver-cd/screwdriver/issues/979
-smart_run mkfifo -m 666 /opt/sd/emitter
+smart_run mkfifo -m 666 /sd/emitter
 
 # Entrypoint
 /opt/sd/tini -- /bin/sh -c "$@"
