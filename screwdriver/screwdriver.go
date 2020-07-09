d23e1cec3e540030f35fad81519bc646d6a5735e
kkisic <amareamo.pxhxc@gmail.com>
2020-07-09 15:09:22 +0900

    rename env

--- screwdriver/screwdriver.go
+++ screwdriver/screwdriver.go
@@ -76,13 +76,13 @@ type api struct {
 // New returns a new API object
 func New(url, token string) (API, error) {
 	// read config from env variables
-	if strings.TrimSpace(os.Getenv("LAUNCHER_SDAPI_TIMEOUT_SECS")) != "" {
-		apiTimeout, _ := strconv.Atoi(os.Getenv("LAUNCHER_SDAPI_TIMEOUT_SECS"))
+	if strings.TrimSpace(os.Getenv("SDAPI_TIMEOUT_SECS")) != "" {
+		apiTimeout, _ := strconv.Atoi(os.Getenv("SDAPI_TIMEOUT_SECS"))
 		httpTimeout = time.Duration(apiTimeout) * time.Second
 	}
 
-	if strings.TrimSpace(os.Getenv("LAUNCHER_SDAPI_MAXRETRIES")) != "" {
-		maxRetries, _ = strconv.Atoi(os.Getenv("LAUNCHER_SDAPI_MAXRETRIES"))
+	if strings.TrimSpace(os.Getenv("SDAPI_MAXRETRIES")) != "" {
+		maxRetries, _ = strconv.Atoi(os.Getenv("SDAPI_MAXRETRIES"))
 	}
 
 	retryClient := retryablehttp.NewClient()
