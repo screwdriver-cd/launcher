1e455ccaa58abaf2e6cc126b31902ef18aa4786a
akanechi <akanechi@yahoo-corp.jp>
2023-11-07 10:58:37 +0900

    Remove unnecessary echo

--- executor/executor.go
+++ executor/executor.go
@@ -287,7 +287,7 @@ func Run(path string, env []string, emitter screwdriver.Emitter, build screwdriv
 			"EXITCODE=$?; " +
 			exportEnvCmd +
 			"echo $SD_STEP_ID $EXITCODE; }", //mv newfile to file
-		"trap finish ABRT EXIT;\n\necho ;\n",
+		"trap finish ABRT EXIT;\n\n",
 	}
 
 	setupReader := bufio.NewReader(f)
